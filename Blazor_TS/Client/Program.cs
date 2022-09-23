ed02f513a1a0baa37a52125825d70077f42811af
leonardosasso <ljscodex@hotmail.com>
2022-09-23 07:49:38 -0300

    initial

--- Blazor_TS/Client/Program.cs
+++ Blazor_TS/Client/Program.cs
@@ -2,9 +2,11 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazor_TS.Client;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
